38b74b5a212b156bab185285bb61b90574eb76ed
Allan19Prieto <allanbanilla@gmail.com>
2026-08-06 21:29:25 -0600

    Probando octave

--- semana-1/octave/script01.m
+++ semana-1/octave/script01.m
@@ -1,16 +1,10 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% CE1111: Análisis Numérico para Ingeniería
-% Escuela de Ingeniería en Computadores
-%
-% Archivo de ejemplo para el curso.
-%
-% Autor: Dr. Juan Pablo Soto Quirós
-% Todos los ejemplos de este archivo fueron creados por el autor con fines
-% exclusivamente didácticos para apoyar el proceso de enseñanza y
-% aprendizaje del curso CE1111: Análisis Numérico para Ingeniería.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Inicialización del entorno de trabajo
+
+% Terminar con ; cada comandos
+% Octave disdingue entre mayuscula y minuscula
+% Instalar oaquetes con: pkg install nombrePaquete
+
 
 % Limpiar la ventana de comandos.
 clc;
