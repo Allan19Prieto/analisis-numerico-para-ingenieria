3d9aa2210ffe5391f3374ee6c4e79c9f60780e30
Allan19Prieto <allanbanilla@gmail.com>
2026-09-10 10:17:31 -0600

    feat: metodo de la secante

--- ejemplos_octave/newtonRaphson.m
+++ ejemplos_octave/newtonRaphson.m
@@ -2,14 +2,20 @@ function newtonRaphson ()
     clc; clear;
 
     f = 'exp(x)-2*x-10';
+    fp = 'exp(x)-2';
     x0 = 1;
     tol = 1e-10;
     iterMax = 1000;
 
-    disp('----- Utilizando simbolic---')
+    disp('----- Utilizando simbolic ---')
     tic
     [xk,k,erk,conv] = nr(f,x0,tol,iterMax)
     t1=toc
+
+    disp('----- Derivada Manual ---')
+    tic
+    [xk,k,erk,conv] = nrD(f,fp,x0,tol,iterMax)
+    t2=toc
 
   end
 
@@ -29,6 +35,28 @@ function newtonRaphson ()
       while (k<iterMax && erk>tol)
         k = k + 1;
         xk = xk - (fn(xk)/fnD(xk));
+        erk = abs(fn(xk));
+      endwhile
+
+      if k == iterMax
+        conv = 'No converge';
+       else
+        conv = 'Converge';
+      endif
+  end
+
+  function [xk,k,erk,conv] = nrD(f,fp,x0,tol,iterMax)
+
+      fn = str2func(['@(x)', f]);
+      fpn = str2func(['@(x)', fp]);
+
+      xk = x0;
+      k = 0;
+      erk = abs(fn(xk));
+
+      while (k<iterMax && erk>tol)
+        k = k + 1;
+        xk = xk - (fn(xk)/fpn(xk));
         erk = abs(fn(xk));
       endwhile
 
